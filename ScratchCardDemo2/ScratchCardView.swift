bc038f115c818f30337d65d6a17380345625e4d5
huangxionging <huangxionging@163.com>
2015-06-04 18:17:54 +0800

    swift 开发的
    熟悉 swift

---
+++ ScratchCardDemo2/ScratchCardView.swift
@@ -0,0 +1,21 @@
+//
+//  ScratchCardView.swift
+//  ScratchCardDemo2
+//
+//  Created by huangxiong on 15/6/4.
+//  Copyright (c) 2015年 New_Life. All rights reserved.
+//
+
+import UIKit
+
+class ScratchCardView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ScratchCardDemo2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ScratchCardDemo2
+//
+//  Created by huangxiong on 15/6/4.
+//  Copyright (c) 2015年 New_Life. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var scratchCardView: (ScratchCardView);
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
